9395de19c2f6e59bf76b8f54e87b979301e8a1d2
Arda “Adu” Telsiz <98750793+rdlRisus@users.noreply.github.com>
2023-08-30 20:57:23 +0300

    Add files via upload

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,4 +1,6 @@
+xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --rate 165 &
 $HOME/.dwm/feh.sh &
-discord &
+flatpak run com.discordapp.Discord &
+# lxsession &
 slstatus &
 xcompmgr &
