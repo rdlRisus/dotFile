199b540d1b1da2d9ae71f1d9611a7ee28b6ce8aa
Arda “Adu” Telsiz <98750793+rdlRisus@users.noreply.github.com>
2023-08-31 11:13:32 +0300

    dbus fix

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,4 +1,5 @@
 xrandr --output DisplayPort-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --rate 165 &
+$HOME/.dwm/dbus.sh &
 feh --randomize --bg-fill .wall/* &
 flatpak run com.discordapp.Discord &
 # lxsession &
